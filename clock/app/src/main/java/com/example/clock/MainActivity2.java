acc3fb338ee2063b8c4bbd1137d225620c31d197
Tsunajenny <tsunajenny0723@gmail.com>
2019-04-25 23:40:35 +0800

    clock

---
+++ clock/app/src/main/java/com/example/clock/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.clock;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
